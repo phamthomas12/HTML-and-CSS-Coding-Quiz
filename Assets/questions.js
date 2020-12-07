b2b5b1d8821c2f9fdddee10458b821fddfda3cec
phamthomas12 <phamthomas12@gmail.com>
2020-12-07 18:18:39 +1100

    Working Questions and Answers

--- Assets/questions.js
+++ Assets/questions.js
@@ -1,32 +1,27 @@
 var questions = [
     {
         title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
+        choices: ["strings", "booleans", "alerts", "numbers", "integar"],
         answer: "alerts"
     },
     {
         title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets", "double forward slash"],
         answer: "parentheses"
     },
     {
         title: "Arrays in JavaScript can be used to store ____.",
-        choices: [
-            "numbers and strings",
-            "other arrays",
-            "booleans",
-            "all of the above"
-        ],
+        choices: ["numbers ", "other arrays", "booleans", "strings", "all of the above"],
         answer: "all of the above"
     },
     {
         title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        choices: ["commas", "curly brackets", "quotes", "parentheses", "square brackets"],
         answer: "quotes"
     },
     {
         title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log", "variables"],
         answer: "console.log"
     }
 ];
--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,4 @@
+var startTime = 75;
 var penalty = 10;
 var answers = document.querySelectorAll('#quizSection button');
 var scorelist = [];
@@ -5,9 +6,12 @@ var answerOne = document.getElementById("answerOne");
 var answerTwo = document.getElementById("answerTwo");
 var answerThree = document.getElementById("answerThree");
 var answerFour = document.getElementById("answerFour");
+var answerFour = document.getElementById("answerFive");
 var timer = document.querySelector("#timer");
-var messageDiv = document.querySelector("#message");
-var startTime = 75;
+var messageDiv = document.querySelector("#welcome");
+var i = 0;
+var score = 0;
+var storedScores;
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -19,10 +23,107 @@ function setTime() {
             clearInterval(timerInterval);
             alert("Out of Time");
         }
+        else if (i === questions.length) {
+            clearInterval(timerInterval);
+        }
     }, 1000);
     //return (score)
 }
 
+function questionSetter() {
+
+    answerOne.hidden = false;
+    answerTwo.hidden = false;
+    answerThree.hidden = false;
+    answerFour.hidden = false;
+    answerFive.hidden = false;
+
+
+    if (i === questions.length) {
+        questionEnder();
+    }
+    else {
+        document.getElementById("askingQuestion").textContent = questions[i]["title"];
+        document.getElementById("answerOne").textContent = questions[i]["choices"][0];
+        document.getElementById("answerTwo").textContent = questions[i]["choices"][1];
+        document.getElementById("answerThree").textContent = questions[i]["choices"][2];
+        document.getElementById("answerFour").textContent = questions[i]["choices"][3];
+        document.getElementById("answerFive").textContent = questions[i]["choices"][4];
+    }
+}
+
 document.getElementById("startButton").addEventListener("click", setTime);
+document.getElementById("startButton").addEventListener("click", questionSetter);
+document.getElementById("startButton").addEventListener("click", function () {
+    messageDiv.textContent = "";
+});
+answerOne.hidden = true;
+answerOne.hidden = true;
+answerTwo.hidden = true;
+answerThree.hidden = true;
+answerFour.hidden = true;
+
+
+document.getElementById("answerOne").addEventListener("click", function () {
+    if (questions[i]["choices"][0] === questions[i]["answer"]) {
+        messageDiv.textContent = "Correct!";
+        score++;
+    }
+    else {
+        messageDiv.textContent = "Wrong!";
+        secondsLeft -= 10;
+    }
+    i++;
+    questionSetter();
+})
+document.getElementById("answerTwo").addEventListener("click", function () {
+    if (questions[i]["choices"][1] === questions[i]["answer"]) {
+        messageDiv.textContent = "Correct!";
+        score++;
+    }
+    else {
+        messageDiv.textContent = "Wrong!";
+        startTime -= 10; 
+    }
+    i++;
+    questionSetter();
+})
+document.getElementById("answerThree").addEventListener("click", function () {
+    if (questions[i]["choices"][2] === questions[i]["answer"]) {
+        messageDiv.textContent = "Correct!";
+        score++;
+    }
+    else {
+        messageDiv.textContent = "Wrong!";
+        startTime -= 10; 
+    }
+    i++;
+    questionSetter();
+})
+document.getElementById("answerFour").addEventListener("click", function () {
+    if (questions[i]["choices"][3] === questions[i]["answer"]) {
+        messageDiv.textContent = "Correct!";
+        score++;
+    }
+    else {
+        messageDiv.textContent = "Wrong!";
+        startTime -= 10; 
+    }
+    i++;
+    questionSetter();
+})
+document.getElementById("answerFive").addEventListener("click", function () {
+    if (questions[i]["choices"][4] === questions[i]["answer"]) {
+        messageDiv.textContent = "Correct!";
+        score++;
+    }
+    else {
+        messageDiv.textContent = "Wrong!";
+        startTime -= 10; 
+    }
+    i++;
+    questionSetter();
+})
+
 
 
