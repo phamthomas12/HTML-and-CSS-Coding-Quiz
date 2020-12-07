f3f0bf4e5c131484ce6a8c3da304e32865885b1d
phamthomas12 <phamthomas12@gmail.com>
2020-12-07 23:47:48 +1100

    About done

---
+++ Assets/scoreboard.js
@@ -0,0 +1,45 @@
+var highScoreList = document.querySelector("#highScores");
+var backButton = document.querySelector("#go-back");
+var clearScores = document.querySelector("#clear-highscores");
+
+initScores();
+
+function initScores() {
+    storedScores = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedScores !== null) {
+        scoreList = storedScores;
+    }
+    renderScores();
+}
+function clearScoreboard() {
+    window.localStorage.clear();
+}
+function renderScores() {
+    if (storedScores !== null) {
+        for (i = 0; i < scoreList.length; i++) {
+            var scoreListItem = scoreList[i];
+            var  table = document.createElement("table");
+            var nameCell = document.createElement("scoreLine");
+            var nameCellText = document.createTextNode(scoreListItem.name);
+            var scoreCell = document.createElement("scoreLine");
+            var scoreCellNum = document.createTextNode(scoreListItem.newScore);
+
+            table.setAttribute("index", i);
+            document.getElementById("highScores").appendChild(table);
+            table.appendChild(nameCell);
+            nameCell.appendChild(nameCellText);
+            table.appendChild(scoreCell);
+            scoreCell.appendChild(scoreCellNum);
+
+        }
+    }
+}
+
+clearScores.addEventListener("click", function () {
+    clearScoreboard();
+    window.location.href = "highScores.html";
+})
+backButton.addEventListener("click", function () {
+    window.location.href = "index.html";
+})
--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,12 @@
 var startTime = 75;
 var penalty = 10;
 var answers = document.querySelectorAll('#quizSection button');
-var scorelist = [];
+var scoreList = [];
 var answerOne = document.getElementById("answerOne");
 var answerTwo = document.getElementById("answerTwo");
 var answerThree = document.getElementById("answerThree");
 var answerFour = document.getElementById("answerFour");
-var answerFour = document.getElementById("answerFive");
+var answerFive = document.getElementById("answerFive");
 var timer = document.querySelector("#timer");
 var messageDiv = document.querySelector("#welcome");
 var i = 0;
@@ -22,12 +22,44 @@ function setTime() {
         if (startTime === 0) {
             clearInterval(timerInterval);
             alert("Out of Time");
+            questionEnder();
         }
         else if (i === questions.length) {
             clearInterval(timerInterval);
         }
     }, 1000);
-    //return (score)
+    return (score)
+}
+
+function questionEnder() {
+
+    var scoreTag = document.createElement("initials");
+    var inputTag = document.createElement("input");
+    var submitButton = document.createElement("button");
+    score += startTime ;
+    score = score.toFixed(0);
+    document.getElementById("askingQuestion").textContent = "Quiz Over";
+    answerOne.remove();
+    answerTwo.remove();
+    answerThree.remove();
+    answerFour.remove();
+    answerFive.remove();
+    messageDiv.remove()
+    document.body.children[1].appendChild(scoreTag);
+    document.getElementsByTagName("initials")[0].setAttribute("id", "score");
+    document.getElementById("score").textContent = "Final Score: " + score;
+    document.body.children[1].appendChild(inputTag);
+    document.getElementsByTagName("input")[0].setAttribute("id", "input-field");
+    submitButton.textContent = "Submit";
+    document.body.children[1].appendChild(submitButton);
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        var highScoreText = new Object();
+        highScoreText.name = inputTag.value.trim();
+        highScoreText.newScore = score;
+        storeScores(highScoreText);
+        window.location.href = "highScores.html";
+    });
 }
 
 function questionSetter() {
@@ -52,16 +84,29 @@ function questionSetter() {
     }
 }
 
+function storeScores(highScoreText) {
+    tempArray = JSON.parse(localStorage.getItem("scores"));
+    if (tempArray === null) {
+        scoreList.push(highScoreText);
+        localStorage.setItem("scores", JSON.stringify(scoreList));
+    }
+    else {
+        tempArray.push(highScoreText);
+        localStorage.setItem("scores", JSON.stringify(tempArray));
+    }
+}
+
+
 document.getElementById("startButton").addEventListener("click", setTime);
 document.getElementById("startButton").addEventListener("click", questionSetter);
 document.getElementById("startButton").addEventListener("click", function () {
     messageDiv.textContent = "";
 });
 answerOne.hidden = true;
-answerOne.hidden = true;
 answerTwo.hidden = true;
 answerThree.hidden = true;
 answerFour.hidden = true;
+answerFive.hidden = true;
 
 
 document.getElementById("answerOne").addEventListener("click", function () {
